083929a98a9d9ef09dc6915e3c340ba43d7076cf
Chris Timms <chriscotimms@gmail.com>
2022-11-23 14:40:13 +0000

    after foundersandcoders session: removed one layer of abstraction in array and got sort abstraction working as anonymous function.

--- moviedatajs.js
+++ moviedatajs.js
@@ -40,33 +40,35 @@ let movieData = {
   },
 };
 
-//const me = Object.entries(movieData);
-//const meArray = Object.entries(me);
-//console.log("meArray" + meArray);
 
-const me = Object.entries(movieData); // turn overall structure into an array
+// turn overall structure into an array
+const me = Object.entries(movieData); 
+//console.log(me);
 
-me.forEach(([key, value]) => { //add key of upper object into properties with values within object
-  //console.log(key, value);
-  value.name = key;
-  //console.log(value.name);
+//add key of upper object into properties with values within object
+me.forEach(([key, value]) => { 
+value.name = key;
 });
 
-console.log("me summary " + me);
+//turn array of objects of objects into array of objects
+//removing nesting level of objects
+const agregator = [];
+for (var i = 0; i < me.length; i++){
+  agregator[i] = me[i][1];
+}
+console.log(agregator);
 
 
-
-
-
+/*
 const byRating = me.sort((a, b) => { //sorts - still needs to be anonymised
-  return b[1].runtime - a[1].runtime;
+  return b[1].year - a[1].year;
 });
 console.log(byRating[0][0] + byRating[1][0] + byRating[2] + byRating[3]);
 console.log(byRating);
+*/
 
 
-
-
+/*
 addEventListener('load', (event) => { //onload, should be distinct from onclick
 
   for (let i = 0; i < byRating.length; i++) // get length of movielist
@@ -77,186 +79,73 @@ addEventListener('load', (event) => { //onload, should be distinct from onclick
     document.getElementById("container1").appendChild(para);
   }
   });
-
-
-
-
-
-/*
-const MoviesNewSort = me.sort((a,b) => {
-  return a.runtime - b.runtime;
-})
-console.log("MoviesNewSort" + MoviesNewSort);
 */
 
-
-/*
-//(movieData)<-nameofObject--[0-3]<-Film number--[0]<-FilmNameString/[1]=Sub-keys + values, eg .plot
-console.log(Object.entries(movieData)[0][1].runtime);//a working way to access info as an object
-console.log(Object.keys(movieData)[1]);//a working way to access info as an object
-console.log(Object.values(movieData)[1].rating);//a working way to access info as an object
-*/
-
-
-
-//const generalSort = ae.sort(a,b)
-
-/*
-const ratingSorted = [];
-for (var i = 0, f = i+1; i < a.length; i++) {
-if (a[i][1].runtime > a[f][1].runtime) {
-
-    ratingSorted.unshift(a[i][1].runtime);
-    console.log(a[i][1].runtime + 'yes' + i + f);
-
-} else {
-
-    ratingSorted.push(a[i][1].runtime);
-    console.log(a[i][1].runtime + 'no' + i + f);
-} 
-}
-console.log(ratingSorted + "ratings sorted" + i);
-*/
-
-/*
-//Copy array and sort order according to ratings
-let arr = [];
-for (var i = 0; i < a.length; i++) {
-  arr.push({
-      movie: a[i][0],
-      runtime: b[i].runtime
+//using anonymous variables with sort function
+//to be able to call any numerical property to sort
+function sortByProperty(inputArray, property){
+  var inputArrayClone = JSON.parse(JSON.stringify(inputArray))
+  let byRating = inputArrayClone.sort((a, b) => { //sorts - still needs to be anonymised
+    return a[property] - b[property];
   });
+  return byRating; 
 }
-console.log(arr);
-*/
 
+//let genericArray = [{test:1010}, {test:1005}, {test:1075}];
+//let sortedArray = sortByProperty(genericArray, "test");
+//console.log("test ", sortedArray);
+
+//let sortedArray2 = sortByProperty(agregator, "runtime");
+//console.log("rating ", sortedArray2);
+
+let sortedArray4 = sortByProperty(agregator, "year");
+console.log("year ", sortedArray4);
+
+let sortedArray3 = sortByProperty(agregator, "runtime");
+console.log("runtime ", sortedArray3);
 /*
-for (let j = 0; j < arr.length; j++) {
-arr[1].sort(function(a, b) {return b - a});
-}
-console.log(arr + 'sorted');
-*/
-
-//arr.sort(function(a, b) {return b[0].runtime - a[1].runtime });
-//console.log(arr + 'runtime sort');
-
-
-
-
-//console.log(ratingSorted + "ratingsSorted");
-//console.log(ratingSortedCopy + "ratingsSortedCopy");
-
-
-
-
-/*
-let result = [];
-const compareArrays = (a, b) => {
-for (var i = 0; i < a.length; i++) {
-  if (a[i] == b) {
-    result.push(i + 'yes');
-    } else {
-      //result.push(i + 'no');
-    }
-  } 
-  console.log(result);
-}
-
-
-console.log(compareArrays(ratingSortedCopy, ratingSorted[0]));
 */
 
 
 
 
-/*
-function comparativeFunction(c, d) {
-for (var varIndex = 0; varIndex < d.length; varIndex++) {
-  compareArrays(d, c[varIndex]);
-}
-}
-comparativeFunction(ratingSortedCopy, ratingSorted);
-*/
 
 
 /*
-const compareArrays = (a, b) => {
-let result = [];
-for (var i = 0; i < a.length; i++) {
-  if (a[i] == b[i]) {
-    result.push(i);
-  } else {
-    for (var j = 0; i < b.length; j++){
-      if (a[i] == b[j]) {
-        result.push(j);
-      }
-    }
+function updateMovieInfo() {
+  document.querySelectorAll(".movieinfo").forEach(elements => elements.remove()); //remove all children with ".movieinfo" class
+  
 
-  }
+                for (let i = 0; i < byRating.length; i++) // for loop to process selector through moviedata (/me)
+                {
+                  const para = document.createElement("div"); 
+                  para.className = "movieinfo";
+                  para.innerHTML = byRating[i][0] + "<br/><br/>" + byRating[i][1].plot + "<br/><br/>" + byRating[i][1].year + "<br/><br/>Runtime: " + byRating[i][1].runtime + " minutes<br/>" + "Rating: " + byRating[i][1].rating; //verbose, could clean
+                  document.getElementById("container1").appendChild(para);
+                }
+  
+  };
+///*
+// radio buttons
+var rad = document.myForm.selection;
+var prev = null;
+for (var i = 0; i < rad.length; i++) {
+    rad[i].addEventListener('change', function() {
+        if (this !== prev) {
+            prev = this; //update prev 
+            updateMovieInfo(this);
+        }
+
+        console.log(this.value)
+    });
 }
-return result;
-}
-
-console.log(compareArrays(ratingsSortedCopy, ratingSorted));
-*/   
-
-
-//map out change in array, to rearrange display order
-//let newOrder = [];
-//for (let i = 0; i < Object.entries(movieData).length; i++) {
-//  if (ratingSorted[i] === Object.entries(movieData)[i][1].runtime);
-//  newOrder.push(i);
-//} 
-//console.log(newOrder);
-
-
-
-
-
-/*
-
-//check values of ratings key, then output to an array
-let ratingSorted = [];
-for (let i = 0; i < movieList.length; i++) {
-ratingSorted.push(movieData[movieList[i]].rating);
-}
-console.log(ratingSorted);
-
-//Sort order according to ratings
-ratingSorted.sort((a, b) => b - a);
-console.log(ratingSorted);
-
-//find items based on order 
-
-
-
-
-//check values of ratings key, then output to an Object
-const ratingObject = new Object();
-for (let i = 0; i < movieList.length; i++) {
-ratingObject[i] = movieData[movieList[i]].rating;
-}
-console.log(Object.values(ratingObject));
 
 */
 
 
 /*
-
-//get key properties movie strings into an array
-let movieList = Object.keys(movieData);
-console.log(movieList[1]);
-console.log(movieList[0]);
-
-// testing loading elements are working
-addEventListener('load', (event) => {
-
-for (let i = 0; i < movieList.length; i++) // get length of movielist
-{
-  const para = document.createElement("div"); 
-  para.className = "movieinfo";
-  para.innerHTML = movieList[i] + "<br/><br/>" + movieData[movieList[i]].plot + "<br/><br/>" + movieData[movieList[i]].year + "<br/><br/>Runtime: " + movieData[movieList[i]].runtime + " minutes<br/>" + "Rating: " + movieData[movieList[i]].rating;//Object.keys(movieData);
-  document.getElementById("container1").appendChild(para);
-}
-});
+const para = document.createElement("div"); 
+para.className = "movieinfo";
+para.innerHTML = byRating[i][0] + "<br/><br/>" + byRating[i][1].plot + "<br/><br/>" + byRating[i][1].year + "<br/><br/>Runtime: " + byRating[i][1].runtime + " minutes<br/>" + "Rating: " + byRating[i][1].rating; //verbose, could clean
+document.getElementById("container1").appendChild(para);
 */
